7afe2222dde6db3603e4712ffda2b770a26867a0
olles <oliver.werner@stud.h-da.de>
2022-04-08 10:33:39 +0200

    Added RotateAroundMe Script
    Added a script that lets the sphere object rotate around the origin object cube.

---
+++ Internship CC FG/Assets/Scripts/RotateAroundMe.cs
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////
+///
+///  Rotate Around Me Script for Frauenhofer-Gesellschaft (Internship Task)
+///  Oliver Werner
+///  08.04.2022
+/// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundMe : MonoBehaviour
+{
+    private GameObject rotatingObject;
+    private GameObject originObject;
+    private float speed;
+
+    void Start()
+    {
+        rotatingObject = GameObject.Find("Sphere");
+        originObject = GameObject.Find("Cube");
+        speed = 100.0f;
+    }
+
+    
+    void Update()
+    {
+        // The rotating object ("Sphere") is orbiting around its origin ("Cube") with a fixed value ("speed").
+        rotatingObject.transform.RotateAround(originObject.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
